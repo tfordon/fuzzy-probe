84b057ade3d37def946cd93fb264ced7fd3a35ee
Tom Fordon <tom.fordon@gmail.com>
2017-06-08 12:26:05 -0400

    adding a simple example

---
+++ examples/simple-static/runFuzzyProbe.sh
@@ -0,0 +1,1 @@
+../../fuzzy-probe.py simple-test 0x08048b43 0x08048b6c 0x0804a0f0 10
---
+++ examples/simple-static/simple-test.cpp
@@ -0,0 +1,81 @@
+#include <array>
+#include <iostream>
+#include <string>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+static bool checkMessage(char* password){
+    if (
+        password[0] == 'p' &&
+        password[1] == 'a' &&
+        password[2] == 's' &&
+        password[3] == 's' &&
+        password[4] == 'w' &&
+        password[5] == 'o' &&
+        password[6] == 'r' &&
+        password[7] == 'd'
+    ){
+       return true;
+    }
+
+    return false;
+}
+
+static void getMessage(char* input){
+  std::string notRight("not right");
+  notRight.copy(input, 10);
+}
+
+
+
+
+
+
+
+
+
+char message[10];
+
+int main(int argc, char **argv){
+
+  getMessage(message);
+ 
+  //this is where the injection jumps to 
+  bool isMessageBad = checkMessage(message);
+
+  if(isMessageBad){
+    //force a fault if the message is good.  Easier to detect with afl 
+    int i = 1/0;
+  }
+
+  //this is where the injector will exit
+  std::cout << isMessageBad << std::endl;
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int dummy(){
+  read(0,0,0);
+  printf("hello");
+  printf("hello%i", 1);
+  exit(0);
+  return 0;
+}
